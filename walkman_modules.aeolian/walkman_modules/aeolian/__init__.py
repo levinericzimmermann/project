400e6f75acc3c1fdc872d9e468b41c786840b530
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-02-14 23:49:17 +0100

    walkman: Ensure magnets are really turned off once they stoped playing

--- walkman_modules.aeolian/walkman_modules/aeolian/__init__.py
+++ walkman_modules.aeolian/walkman_modules/aeolian/__init__.py
@@ -47,7 +47,15 @@ class String(
             # (0, 50, 100, 155, 100, 50, 0),
             # (100, 125, 150, 180, 150, 125, 100),
             # (220, 230, 240, 255, 240, 230, 220),
-            (240, 245, 250, 255, 250, 245, 240),
+            # (240, 245, 250, 255, 250, 245, 240),
+            #
+            # sehr laut, und direkt, vielleicht sogar lauter
+            # als das erste
+            (0, 255, 255, 255, 255, 255, 0),
+            # Only plucking, no static pitch
+            # (255, 254, 253),
+            # rather static pitch
+            # (99, 99, 99, 100, 100, 100, 100, 101, 101, 102, 102, 102, 102, 103, 103, 103, 103, 102, 101, 100, 99, 98, 99),
         )
         self.envelope_repetition_count_range = (5, 20)
         self.envelope_cycle = itertools.cycle(self.envelope_tuple)
@@ -63,6 +71,21 @@ class String(
         self.mod = pyo.LFO(6, add=1, mul=0.25) + 0.75
         self.metro = pyo.Metro(self.sleep_time * self.mod)
         self.trig_func = pyo.TrigFunc(self.metro, self._set_magnetic_field)
+
+        # We want to be very sure that once the sound ends the magnet is turned
+        # off (control value = 0). Because the frequency of the magnetic field
+        # doesn't necessarily fall onto the fadeout time, the last send control
+        # value is very likely not 0. So we add an extra trigger which waits
+        # until the whole fadeout process is finished and then finally sends a
+        # 0 to the magnet.
+
+        def deactivate_magnet():
+            walkman.constants.LOGGER.debug(f"Deactivate magnet on pin = {self.pin}.")
+            self._set_magnetic_field(0)
+
+        self.stop_trigger = pyo.Trig()
+        self.stop_trigger_function = pyo.TrigFunc(self.stop_trigger, deactivate_magnet)
+
         self.internal_pyo_object_list.extend(
             [self.metro, self.trig_func, self.control_value_count, self.mod]
         )
@@ -88,8 +111,15 @@ class String(
             self.control_value_count.setValue(len(envelope))
             return self._next_control_point()
 
-    def _set_magnetic_field(self):
-        self.board.analog_write(self.pin, int(self._next_control_point()))
+    def _set_magnetic_field(self, value=None):
+        if value is None:
+            value = int(self._next_control_point())
+        walkman.constants.LOGGER.debug(f"Send value {value} on pin = {self.pin}.")
+        self.board.analog_write(self.pin, value)
+
+    def _stop_without_fader(self, wait: float = 0):
+        super()._stop_without_fader(wait=wait)
+        self.stop_trigger.play(delay=wait + 0.1)
 
     def close(self):
         super().close()
