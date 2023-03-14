ed54df5b77df837113c754a0200c6a0f55db5d9f
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-15 00:36:23 +0100

    improve clavichord chords

---
+++ mutwo/project_generators/chords.py
@@ -0,0 +1,188 @@
+import collections
+import itertools
+import typing
+
+import ranges
+
+from mutwo import core_utilities
+from mutwo import music_parameters
+
+
+__all__ = (
+    "Chord",
+    "ChordDifference",
+    "find_chord_tuple",
+    "make_chord_difference_tuple",
+)
+
+
+Chord = collections.namedtuple(
+    "Chord", ("pitch_tuple", "pitch_count", "harmonicity", "ambitus", "fingering_tuple")
+)
+
+ChordDifference = collections.namedtuple(
+    "ChordDifference",
+    (
+        "intersection",
+        "union",
+        "only0",
+        "only1",
+        "intersection_count",
+        "interval_tuple",
+        "harmonicity",
+    ),
+)
+
+
+def make_chord_difference_tuple(
+    chord_tuple0: tuple[Chord, ...],
+    chord_tuple1: tuple[Chord, ...],
+) -> tuple[tuple[Chord, Chord, ChordDifference], ...]:
+    """Compute properties of all possible chord sequences between two chord sets"""
+
+    chord_difference_list = []
+    for chord0, chord1 in itertools.product(chord_tuple0, chord_tuple1):
+        chord_difference_list.append(_make_chord_difference(chord0, chord1))
+    return tuple(chord_difference_list)
+
+
+def _make_chord_difference(chord0: Chord, chord1: Chord):
+    union = tuple(
+        sorted(core_utilities.uniqify_iterable(chord0.pitch_tuple + chord1.pitch_tuple))
+    )
+    intersection, only0, only1 = [], [], []
+    for p in union:
+        if p in chord0.pitch_tuple and p in chord1.pitch_tuple:
+            intersection.append(p)
+        elif p in chord0.pitch_tuple:
+            only0.append(p)
+        else:
+            only1.append(p)
+    interval_tuple = tuple(
+        _interval(p0, p1)
+        for p0, p1 in itertools.product(chord0.pitch_tuple, chord1.pitch_tuple)
+    )
+    harmonicity_delta = chord1.harmonicity - chord0.harmonicity
+    return ChordDifference(
+        tuple(intersection),
+        union,
+        tuple(only0),
+        tuple(only1),
+        len(intersection),
+        interval_tuple,
+        harmonicity_delta,
+    )
+
+
+def find_chord_tuple(
+    picked_pitch_tuple: tuple[music_parameters.JustIntonationPitch, ...] = tuple([]),
+    pitch_tuple: tuple[music_parameters.JustIntonationPitch, ...] = tuple([]),
+    pitch_count_range: ranges.Range = ranges.Range(1, 3),
+    min_harmonicity: typing.Optional[float] = music_parameters.JustIntonationPitch(
+        "7/4"
+    ).harmonicity_simplified_barlow,
+    max_harmonicity: typing.Optional[float] = None,
+    ambitus: music_parameters.abc.PitchAmbitus = music_parameters.OctaveAmbitus(
+        music_parameters.JustIntonationPitch("1/4"),
+        music_parameters.JustIntonationPitch("4/1"),
+    ),
+    instrument: typing.Optional[music_parameters.abc.Instrument] = None,
+    min_interval: typing.Optional[music_parameters.abc.PitchInterval] = None,
+    max_interval: typing.Optional[music_parameters.abc.PitchInterval] = None,
+    **kwargs,
+) -> tuple[Chord, ...]:
+    """Create a tuple of chords which fulfill specific constraints"""
+
+    valid_pitch_tuple = tuple(p for p in pitch_tuple if p in ambitus and p not in picked_pitch_tuple)
+    picked_pitch_count = len(picked_pitch_tuple)
+
+    chord_list = []
+    for pitch_count in range(pitch_count_range.start, pitch_count_range.end):
+        if (difference := pitch_count - picked_pitch_count) >= 0:
+            for partner_tuple in itertools.combinations(valid_pitch_tuple, difference):
+                if chord := _make_chord(
+                    picked_pitch_tuple,
+                    pitch_count,
+                    partner_tuple,
+                    min_harmonicity,
+                    max_harmonicity,
+                    instrument,
+                    min_interval,
+                    max_interval,
+                ):
+                    chord_list.append(chord)
+
+    return tuple(chord_list)
+
+
+def _make_chord(
+    picked_pitch_tuple,
+    pitch_count,
+    partner_pitch_tuple,
+    min_harmonicity,
+    max_harmonicity,
+    instrument,
+    min_interval,
+    max_interval,
+) -> typing.Optional[Chord]:
+    harmonicity_list = []
+    for p0, p1 in tuple(
+        itertools.product(picked_pitch_tuple, partner_pitch_tuple)
+    ) + tuple(itertools.combinations(partner_pitch_tuple, 2)):
+        interval = _interval(p0, p1)
+        cents = interval.interval
+        if min_interval and min_interval > cents:
+            return
+        if max_interval and max_interval < cents:
+            return
+        harmonicity_list.append(_harmonicity(interval))
+    harmonicity = sum(harmonicity_list) / len(harmonicity_list)
+    if max_harmonicity and harmonicity > max_harmonicity:
+        return
+    if min_harmonicity and harmonicity < min_harmonicity:
+        return
+    pitch_tuple = tuple(sorted(picked_pitch_tuple + partner_pitch_tuple))
+    fingering_tuple = None
+    if instrument:
+        try:
+            fingering_tuple = instrument.pitch_sequence_to_fingering_tuple(pitch_tuple)
+        except AttributeError:
+            pass
+        else:
+            if not fingering_tuple:
+                return
+    if len(pitch_tuple) > 1:
+        ambitus = music_parameters.OctaveAmbitus(pitch_tuple[0], pitch_tuple[-1])
+    else:
+        ambitus = music_parameters.OctaveAmbitus(pitch_tuple[0], pitch_tuple[0] + music_parameters.JustIntonationPitch('2/1'))
+    return Chord(
+        pitch_tuple,
+        pitch_count,
+        harmonicity,
+        ambitus,
+        fingering_tuple,
+    )
+
+
+# Catch intervals for faster computation
+def _interval(p0, p1):
+    key = (p0.exponent_tuple, p1.exponent_tuple)
+    try:
+        return PITCH_TUPLE_TO_INTERVAL[key]
+    except KeyError:
+        i = PITCH_TUPLE_TO_INTERVAL[key] = p0 - p1
+        return i
+
+
+# Catch harmonicity for faster computation
+def _harmonicity(p):
+    key = p.exponent_tuple
+    try:
+        return INTERVAL_TO_HARMONICITY[key]
+    except KeyError:
+        h = INTERVAL_TO_HARMONICITY[key] = p.harmonicity_simplified_barlow
+        return h
+
+
+PITCH_TUPLE_TO_INTERVAL = {}
+INTERVAL_TO_HARMONICITY = {}
--- project/entries/dynamic/modal/chords-clavichord.py
+++ project/entries/dynamic/modal/chords-clavichord.py
@@ -7,6 +7,7 @@ from mutwo import core_events
 from mutwo import music_events
 from mutwo import music_parameters
 from mutwo import project_events
+from mutwo import project_generators
 from mutwo import project_parameters
 from mutwo import timeline_interfaces
 
@@ -29,26 +30,13 @@ def main(
     orchestration = context.orchestration
     instrument = orchestration[0]
     scale = context.modal_event.scale
+    pitch = context.modal_event.pitch
 
+    sequential_event = make_sequential_event(instrument, scale, pitch, random)
     simultaneous_event = core_events.SimultaneousEvent(
         [
             core_events.TaggedSimultaneousEvent(
-                [
-                    core_events.SequentialEvent(
-                        [
-                            music_events.NoteLike(
-                                [
-                                    random.choice(instrument.pitch_tuple),
-                                    random.choice(instrument.pitch_tuple),
-                                    random.choice(instrument.pitch_tuple),
-                                ],
-                                volume="p",
-                                duration=random.choice([1, 1.5, 0.75]),
-                            )
-                            for _ in range(random.choice([2, 3, 4]))
-                        ]
-                    )
-                ],
+                [sequential_event],
                 tag=instrument.name,
             )
         ]
@@ -57,9 +45,139 @@ def main(
     duration = context.modal_event.clock_event.duration
     start_range = ranges.Range(duration * 0.3, duration * 0.35)
     end_range = ranges.Range(duration * 0.65, duration * 0.7)
-    # start_range = ranges.Range(duration * 0.04, duration * 0.045)
-    # end_range = ranges.Range(duration * 0.955, duration * 0.9999999999999999996)
 
     return timeline_interfaces.EventPlacement(
         simultaneous_event, start_range, end_range
     ).move_by(context.start)
+
+
+def make_sequential_event(instrument, scale, pitch, random):
+    chord_count = random.choice([1, 2, 3, 4])
+
+    scale_degree = scale.pitch_to_scale_degree(pitch)
+    is_chord_harmonic_tuple = make_is_chord_harmonic_tuple(chord_count)
+    chord_ambitus_tuple = make_chord_ambitus_tuple(chord_count, random)
+    pitch_count_tuple = make_pitch_count_tuple(chord_count, random)
+    main_pitch_tuple = make_main_pitch_tuple(
+        scale_degree, scale, chord_count, chord_ambitus_tuple, random
+    )
+    pitch_tuple = instrument.pitch_tuple
+
+    chord_tuple = make_chord_tuple(
+        pitch_tuple,
+        is_chord_harmonic_tuple,
+        main_pitch_tuple,
+        pitch_count_tuple,
+        chord_ambitus_tuple,
+        random,
+    )
+
+    sequential_event = core_events.SequentialEvent([])
+    for pitch_tuple in chord_tuple:
+        n = music_events.NoteLike(
+            pitch_tuple, duration=random.choice([1, 0.75, 1.5, 2]), volume="p"
+        )
+        sequential_event.append(n)
+    return sequential_event
+
+
+def make_chord_tuple(
+    pitch_tuple,
+    is_chord_harmonic_tuple,
+    main_pitch_tuple,
+    pitch_count_tuple,
+    chord_ambitus_tuple,
+    random,
+):
+    chord_list = []
+    for is_chord_harmonic, main_pitch, pitch_count, ambitus in zip(
+        is_chord_harmonic_tuple,
+        main_pitch_tuple,
+        pitch_count_tuple,
+        chord_ambitus_tuple,
+    ):
+        if is_chord_harmonic:
+            min_interval = music_parameters.JustIntonationPitch("7/6")
+            min_harmonicity = music_parameters.JustIntonationPitch(
+                "3/2"
+            ).harmonicity_simplified_barlow
+            max_harmonicity = None
+        else:
+            min_interval = music_parameters.JustIntonationPitch("10/9")
+            min_harmonicity = music_parameters.JustIntonationPitch(
+                "5/4"
+            ).harmonicity_simplified_barlow
+            max_harmonicity = None
+
+        chord_tuple = project_generators.find_chord_tuple(
+            (main_pitch,),
+            pitch_tuple,
+            ranges.Range(pitch_count, pitch_count + 1),
+            min_harmonicity,
+            max_harmonicity,
+            ambitus,
+            min_interval,
+        )
+        if chord_tuple:
+            chord_list.append(random.choice(chord_tuple)[0])
+        else:
+            chord_list.append([])
+
+    return tuple(chord_list)
+
+
+def make_main_pitch_tuple(
+    scale_degree, scale, chord_count, chord_ambitus_tuple, random
+) -> tuple[music_parameters.JustIntonationPitch, ...]:
+    scale_degree_count = len(set(scale.scale_degree_tuple))
+    assert scale_degree_count == 5
+    if scale_degree == 0:
+        main_pitch_cycle = itertools.cycle((0,))
+    else:
+        low, high = ((scale_degree + n) % scale_degree_count for n in (-1, 1))
+        main_pitch_cycle = itertools.cycle((scale_degree, low, scale_degree, high))
+
+    main_pitch_tuple = tuple(
+        reversed(
+            [
+                random.choice(
+                    chord_ambitus.get_pitch_variant_tuple(
+                        scale.scale_index_to_pitch(next(main_pitch_cycle))
+                    )
+                )
+                for chord_ambitus in chord_ambitus_tuple
+            ]
+        )
+    )
+    return main_pitch_tuple
+
+
+def make_is_chord_harmonic_tuple(chord_count):
+    is_chord_harmonic_cycle = itertools.cycle((True, False))
+    return tuple(
+        reversed(tuple(next(is_chord_harmonic_cycle) for _ in range(chord_count)))
+    )
+
+
+def make_chord_ambitus_tuple(chord_count, random):
+    return tuple(random.choice(ambitus_tuple) for _ in range(chord_count))
+
+
+ambitus_tuple = (
+    music_parameters.OctaveAmbitus(
+        music_parameters.JustIntonationPitch("3/4"),
+        music_parameters.JustIntonationPitch("3/1"),
+    ),
+    music_parameters.OctaveAmbitus(
+        music_parameters.JustIntonationPitch("1/2"),
+        music_parameters.JustIntonationPitch("2/1"),
+    ),
+    music_parameters.OctaveAmbitus(
+        music_parameters.JustIntonationPitch("1/1"),
+        music_parameters.JustIntonationPitch("4/1"),
+    ),
+)
+
+
+def make_pitch_count_tuple(chord_count, random):
+    return tuple(random.choice([2, 3, 4]) for _ in range(chord_count))
