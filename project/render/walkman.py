6177299d7d49144df54c9ce1f7dfb39a193c9b2a
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-15 15:37:15 +0100

    cosmetics, black

--- project/render/walkman.py
+++ project/render/walkman.py
@@ -7,6 +7,6 @@ import walkmanio
 def walkman(simultaneous_event, d):
     mutwo2walkman = project_converters.SequentialEventToWalkmanEventTuple()
     sequence_list = []
-    for s in simultaneous_event['aeolian harp']:
+    for s in simultaneous_event["aeolian harp"]:
         sequence_list.append(mutwo2walkman(s))
     walkmanio.export(project.constants.WALKMAN_DATA_PATH, d, tuple(sequence_list))
