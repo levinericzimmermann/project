f10873435c4e0f2e4e226767c95b4189a83c3e8f
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-15 13:14:41 +0100

    main: render all event parts of april

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 import datetime
 
-from astral import LocationInfo
+# First import project to activate constant + patches
+import project
+
+from astral import LocationInfo, sun, Depression
 
 from mutwo import core_converters
 from mutwo import diary_interfaces
 from mutwo import project_converters
 
-import project
 
+def make_part(location_info, d, day_light):
+    if allowed_date_list and not any(
+        [
+            d.day == d2.day and d.month == d2.month and d.year == d2.year
+            for d2 in allowed_date_list
+        ]
+    ):
+        return
+    if allowed_day_light_list and day_light not in allowed_day_light_list:
+        return
+    astral_event = project_converters.DatetimeToSimultaneousEvent(
+        location_info
+    ).convert(d)
+    clock_tuple = project_converters.AstralEventToClockTuple(
+        project_converters.AstralConstellationToOrchestration(
+            project.constants.MOON_PHASE_TO_INTONATION,
+            project.constants.SUN_LIGHT_TO_PITCH_INDEX_TUPLE,
+            project.constants.MOON_LIGHT_TO_PITCH_INDEX_TUPLE,
+        ),
+        project_converters.AstralConstellationToScale(
+            project.constants.MOON_PHASE_TO_INTONATION,
+            project.constants.SUN_LIGHT_TO_PITCH_INDEX_TUPLE,
+            project.constants.MOON_LIGHT_TO_PITCH_INDEX_TUPLE,
+        ),
+    ).convert(astral_event)
+
+    project.render.midi(clock_tuple)
+    if day_light == "dawn":
+        project.render.notation(clock_tuple)
+
+
+allowed_date_list = [datetime.datetime(2023, 4, 30)]
+allowed_day_light_list = ["dawn"]
 
 if __name__ == "__main__":
-    # ==============================
-    # Fast imitation of what happens inside of walkman:
-    #
-    #   - check current time
-    #   - create from this time clock_tuple (based on entries and converter)
-    #   - render this clock_tuple to real-world sounds
-    #
-    # ==============================
-
     location_info = LocationInfo(
         name="Essen",
         region="NRW",
@@ -27,25 +53,13 @@ if __name__ == "__main__":
         longitude=7.0115552,
     )
 
-    d = datetime.datetime.now(location_info.tzinfo)
-    astral_event = project_converters.DatetimeToSimultaneousEvent(
-        location_info
-    ).convert(d)
     with diary_interfaces.open():
-        clock_tuple = project_converters.AstralEventToClockTuple(
-            project_converters.AstralConstellationToOrchestration(
-                project.constants.MOON_PHASE_TO_INTONATION,
-                project.constants.SUN_LIGHT_TO_PITCH_INDEX_TUPLE,
-                project.constants.MOON_LIGHT_TO_PITCH_INDEX_TUPLE,
-            ),
-            project_converters.AstralConstellationToScale(
-                project.constants.MOON_PHASE_TO_INTONATION,
-                project.constants.SUN_LIGHT_TO_PITCH_INDEX_TUPLE,
-                project.constants.MOON_LIGHT_TO_PITCH_INDEX_TUPLE,
-            ),
-        ).convert(astral_event)
-
-    print(clock_tuple)
-
-    project.render.midi(clock_tuple)
-    project.render.notation(clock_tuple)
+        april = tuple(datetime.datetime(2023, 4, d) for d in range(1, 31))
+        for day in april:
+            print(f"RENDER '{day}'!")
+            s = sun.sun(
+                location_info.observer, date=day, dawn_dusk_depression=Depression.CIVIL
+            )
+            for day_light in ("dawn", "sunrise", "sunset", "dusk"):
+                d = s[day_light]
+                make_part(location_info, d, day_light)
