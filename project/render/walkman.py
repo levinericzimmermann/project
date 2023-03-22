183f75c7c27067adf23314f2b9e88cc22de4c108
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-22 20:00:56 +0100

    walkman: Allow non-existent entry (good for development)

--- project/render/walkman.py
+++ project/render/walkman.py
@@ -1,3 +1,5 @@
+import warnings
+
 from mutwo import project_converters
 
 import project
@@ -7,6 +9,10 @@ import walkmanio
 def walkman(simultaneous_event, d):
     mutwo2walkman = project_converters.SequentialEventToWalkmanEventTuple()
     sequence_list = []
-    for s in simultaneous_event["aeolian harp"]:
-        sequence_list.append(mutwo2walkman(s))
+    try:
+        for s in simultaneous_event["aeolian harp"]:
+            sequence_list.append(mutwo2walkman(s))
+    except KeyError:
+        warnings.warn(f"No aeolian harp event found for {d}!")
+        return
     walkmanio.export(project.constants.WALKMAN_DATA_PATH, d, tuple(sequence_list))
