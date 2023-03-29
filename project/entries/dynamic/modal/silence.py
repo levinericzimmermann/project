681609a636ed0d93f01ccaa3948b40ae90104571
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-29 15:01:19 +0200

    entries/dynamic: prohibit silence in case of low energy level

--- project/entries/dynamic/modal/silence.py
+++ project/entries/dynamic/modal/silence.py
@@ -1,2 +1,10 @@
+def is_supported(context, **kwargs):
+    try:
+        assert context.energy < 8
+    except AssertionError:
+        return False
+    return True
+
+
 def main(context, **kwargs):
     return
