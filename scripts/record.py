7beff26bcf3be8cd00dcf0665077a96097d6eed6
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-04-21 14:02:30 +0200

    record: use ogg

--- scripts/record.py
+++ scripts/record.py
@@ -5,18 +5,19 @@ import time
 import pyo
 
 channel_count = 4
-server = (
-    pyo.Server(
-        jackname="10-1-record", audio="jack", ichnls=channel_count, nchnls=channel_count
-    )
-    .boot()
-    .start()
+server = pyo.Server(
+    jackname="10-1-record", audio="jack", ichnls=channel_count, nchnls=channel_count
 )
+server.setJackAuto(False, False)
+
+server.boot().start()
+
 input_tuple = tuple(pyo.Input(n) for n in range(channel_count))
 now = datetime.datetime.now().isoformat()
 filename = f"etc/recordings/{now[:13]}_{now[14:16]}"
 record_tuple = tuple(
-    pyo.Record(inp, f"{filename}_{i}.wav", sampletype=4, fileformat=0, chnls=1)
+    # pyo.Record(inp, f"{filename}_{i}.wav", sampletype=4, fileformat=0, chnls=1)
+    pyo.Record(inp, f"{filename}_{i}.ogg", sampletype=4, fileformat=7, chnls=1)
     for i, inp in enumerate(input_tuple)
 )
 
