3de6b0d4af81eb9afa922a9b176f5d22acf19999
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-04-30 11:07:39 +0200

    structure: Vary duration of each context depending on gatra position

--- project/clocks.py
+++ project/clocks.py
@@ -1,5 +1,6 @@
-from quicktions import Fraction as f
+import quicktions as fractions
 
+import numpy as np
 import ranges
 
 from mutwo import clock_events
@@ -10,20 +11,17 @@ from mutwo import core_parameters
 def apply_clock_events(
     modal_0_sequential_event: core_events.SequentialEvent[clock_events.ModalEvent0],
 ):
+    random = np.random.default_rng(seed=1)
     filtered_seq = tuple(
         filter(
             lambda e: isinstance(e, clock_events.ModalEvent0), modal_0_sequential_event
         )
     )
-    d = core_parameters.DirectDuration
     clock_event_list = []
-    for modal_event_0 in filtered_seq:
-        if getattr(modal_event_0, "is_end", False):
-            duration_range = ranges.Range(d(f(10, 16)), d(f(30, 16)))
-        else:
-            duration_range = ranges.Range(d(f(30, 16)), d(f(60, 16)))
+    for index, modal_event_0 in enumerate(filtered_seq):
+        duration = index_to_duration(index, random)
         clock_event = clock_events.ClockEvent(
-            [core_events.SequentialEvent([core_events.SimpleEvent(duration_range.end)])]
+            [core_events.SequentialEvent([core_events.SimpleEvent(duration)])]
         )
         clock_event_list.append(clock_event)
     for modal_event0, clock_event in zip(filtered_seq, clock_event_list):
@@ -32,3 +30,28 @@ def apply_clock_events(
         modal_event0.control_event = core_events.SequentialEvent(
             [core_events.SimpleEvent(1)]
         )
+
+
+def index_to_duration(index, random):
+    duration_range = index_to_duration_range(index)
+    return fractions.Fraction(
+        # Explicit cast to builtin int to avoid later
+        # problems with abjad which can only handle builtin
+        # int and not numpy int.
+        int(random.integers(duration_range.start, duration_range.end)), 16
+    )
+
+
+def index_to_duration_range(index):
+    r = ranges.Range
+    match index % 4:
+        case 0:
+            return r(20, 35)
+        case 1:
+            return r(30, 55)
+        case 2:
+            return r(20, 35)
+        case 3:
+            return r(45, 65)
+        case _:
+            raise RuntimeError()
