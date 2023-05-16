a13f479a30249164cd9bc81041f3af7dd2292937
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-05-16 11:08:55 +0200

    entries/clock/modal0: vary duration/position according to gatra index

--- project/entries/dynamic/clocks/modal0.py
+++ project/entries/dynamic/clocks/modal0.py
@@ -31,24 +31,32 @@ def main(context, tremolo_middle, tremolo_long, grace, hit, random, **kwargs):
             clock_event = hit(
                 instrument_index_tuple=[random.choice((2, 3, 4))], **kwargs
             )
+            position = 0
+            real_duration = fractions.Fraction(20, 16)
         case 1:
             clock_event = tremolo_middle(instrument_index_tuple=[4], **kwargs)
+            position = 0
+            real_duration = fractions.Fraction(21, 16)
         case 2:
             clock_event = grace(**kwargs)
+            position = 0.25
+            real_duration = fractions.Fraction(20, 16)
         case 3:
             clock_event = tremolo_long(
                 instrument_index_tuple=[2], **kwargs
             ).concatenate_by_index(
                 hit(instrument_index_tuple=[random.choice((2, 3, 4))], **kwargs)
             )
+            position = 0.85
+            real_duration = fractions.Fraction(25, 16)
         case _:
             raise RuntimeError()
 
     duration = context.modal_event.clock_event.duration
-    real_duration = fractions.Fraction(25, 16)
     if real_duration > duration:
-        real_duration = duration
-    start_range, end_range = project_utilities.get_ranges(real_duration, duration, 0)
+        real_duration = duration - fractions.Fraction(1, 16)
+
+    start_range, end_range = project_utilities.get_ranges(real_duration, duration, position)
 
     return timeline_interfaces.EventPlacement(
         core_events.SimultaneousEvent([clock_event]), start_range, end_range
