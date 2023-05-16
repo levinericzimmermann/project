96638d83b82e1f724c426fc7198e8030518cf9af
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-05-16 11:09:17 +0200

    scale-event-chord-instruments: better distribution of chord on instruments

--- project/entries/dynamic/modal/scale-event-chord-instruments.py
+++ project/entries/dynamic/modal/scale-event-chord-instruments.py
@@ -13,12 +13,13 @@ def is_supported(context, alternating_scale_chords, **kwargs):
         assert len(orchestration) > 1
         for i in orchestration:
             assert isinstance(i, music_parameters.abc.PitchedInstrument)
+        assert context.index % 2 == 0
     except AssertionError:
         return False
     return alternating_scale_chords.is_supported(context, **kwargs)
 
 
-def main(context, alternating_scale_chords, **kwargs):
+def main(context, alternating_scale_chords, random, **kwargs):
     chord_tuple = alternating_scale_chords(context, **kwargs)
 
     modal_event_to_convert = context.modal_event
@@ -28,7 +29,7 @@ def main(context, alternating_scale_chords, **kwargs):
     if real_duration > duration:
         real_duration = duration
 
-    start_range, end_range = project_utilities.get_ranges(real_duration, duration, 0.5)
+    start_range, end_range = project_utilities.get_ranges(real_duration, duration, 0.35)
 
     simultaneous_event = core_events.SimultaneousEvent(
         [
@@ -40,22 +41,49 @@ def main(context, alternating_scale_chords, **kwargs):
     )
 
     for chord in chord_tuple:
-        distribute_chord(chord, simultaneous_event, context.orchestration)
+        duration = random.choice([1, 1.5, 0.75])
+        distribute_chord(
+            duration, chord, simultaneous_event, context.orchestration, random
+        )
 
     return timeline_interfaces.EventPlacement(
         simultaneous_event, start_range, end_range
     ).move_by(context.start)
 
 
-def distribute_chord(chord, simultaneous_event, orchestration):
+def distribute_chord(duration, chord, simultaneous_event, orchestration, random):
+    instrument_pitch_data_list = []
     for instrument in orchestration:
-        added = False
+        playable_pitch_count = 0
+        pitch_variant_list = []
         for p in chord:
-            t = instrument.get_pitch_variant_tuple(p)
-            if added := t:
-                n = music_events.NoteLike(t[len(t) // 2], 1, "p")
-                simultaneous_event[instrument.name][0].append(n)
-                break
-        if not added:
-            n = music_events.NoteLike([], 1, "p")
-            simultaneous_event[instrument.name][0].append(n)
+            if t := instrument.get_pitch_variant_tuple(p):
+                playable_pitch_count += 1
+            pitch_variant_list.append(t)
+        instrument_pitch_data_list.append(
+            (instrument, playable_pitch_count, pitch_variant_list)
+        )
+
+    pitch_count_dict = {p.exponent_tuple: 0 for p in chord}
+    for instrument, _, pitch_variant_list in sorted(
+        instrument_pitch_data_list, key=lambda d: d[1]
+    ):
+        pitch_variant_options_list = []
+        for p, pitch_variant_tuple in zip(chord, pitch_variant_list):
+            if pitch_variant_tuple:
+                pitch_variant_options_list.append(
+                    (pitch_count_dict[p.exponent_tuple], p, pitch_variant_tuple)
+                )
+        if pitch_variant_options_list:
+            _, p, pitch_variant_tuple = sorted(
+                pitch_variant_options_list, key=lambda d: d[0]
+            )[0]
+            pitch_count_dict[p.exponent_tuple] += 1
+            n = music_events.NoteLike(random.choice(pitch_variant_tuple), duration, "pp")
+            if instrument.name in ("v",):
+                n.playing_indicator_collection.string_contact_point.contact_point = (
+                    "pizzicato"
+                )
+        else:
+            n = music_events.NoteLike([], duration, "p")
+        simultaneous_event[instrument.name][0].append(n)
