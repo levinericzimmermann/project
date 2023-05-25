76a771e48bc683ba7824b5dd13524c9346cbd05e
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-05-25 12:45:13 +0200

    converters/clocks: Apply mutwo.timeline fixes

--- mutwo/project_converters/clocks.py
+++ mutwo/project_converters/clocks.py
@@ -14,8 +14,8 @@ class ClockLineToSimultaneousEvent(clock_converters.ClockLineToSimultaneousEvent
     def _event_placement_to_event(
         self, *args, **kwargs
     ) -> core_events.SimultaneousEvent:
-        event = super()._event_placement_to_event(*args, **kwargs)
-        d = event.duration
-        r = getattr(event, "repetition_count_range", ranges.Range(1, 2))
-        repetition_count = self._random.choice(range(r.start, r.end))
-        return event.repeat(repetition_count - 1, mutate=False).set("duration", d)
+        if (event := super()._event_placement_to_event(*args, **kwargs)):
+            d = event.duration
+            r = getattr(event, "repetition_count_range", ranges.Range(1, 2))
+            repetition_count = self._random.choice(range(r.start, r.end))
+            return event.repeat(repetition_count - 1, mutate=False).set("duration", d)
