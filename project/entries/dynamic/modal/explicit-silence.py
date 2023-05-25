f87b73c01e1cdcb71b101c6d8a2d040708a40bcf
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-05-25 12:44:46 +0200

    explicit-silence: Fix fermata notation

--- project/entries/dynamic/modal/explicit-silence.py
+++ project/entries/dynamic/modal/explicit-silence.py
@@ -19,28 +19,48 @@ def main(context, **kwargs) -> timeline_interfaces.EventPlacement:
 
     duration = context.modal_event.clock_event.duration
 
-    real_duration = fractions.Fraction(10, 16)
+    real_duration = fractions.Fraction(20, 16)
     if real_duration > duration:
-        real_duration = duration
-    start_range, end_range = project_utilities.get_ranges(real_duration, duration, 0.5)
+        real_duration = duration - fractions.Fraction(2, 16)
+    start_range, end_range = project_utilities.get_ranges(real_duration, duration, 0.95)
 
     simultaneous_event = core_events.SimultaneousEvent([])
     for instrument in orchestration:
-        note_like = music_events.NoteLike(duration=1)
-        # Any of
-        #
-        #   "shortfermata",
-        #   "fermata",
-        #   "longfermata",
-        #   "verylongfermata",
-        #
-        note_like.playing_indicator_collection.fermata.type = "longfermata"
-        sequential_event = core_events.SequentialEvent([note_like])
+        if instrument.name == "pclock":
+            continue
+
         tagged_simultaneous_event = core_events.TaggedSimultaneousEvent(
-            [sequential_event], tag=instrument.name
+            [], tag=instrument.name
         )
+
+        match instrument.name:
+            case "harp":
+                staff_count = 2
+            case _:
+                staff_count = 1
+
+        for _ in range(staff_count):
+            note_like = music_events.NoteLike(duration=1)
+            try:
+                fermata_type = FERMATA_TYPE_TUPLE[abs(context.energy) - 1]
+            except IndexError:
+                fermata_type = FERMATA_TYPE_TUPLE[-1]
+            note_like.playing_indicator_collection.fermata.type = fermata_type
+            sequential_event = core_events.SequentialEvent([note_like])
+            tagged_simultaneous_event.append(sequential_event)
+
+        assert tagged_simultaneous_event
+
         simultaneous_event.append(tagged_simultaneous_event)
 
     return timeline_interfaces.EventPlacement(
         simultaneous_event, start_range, end_range
     ).move_by(context.start)
+
+
+FERMATA_TYPE_TUPLE = (
+    "shortfermata",
+    "fermata",
+    "longfermata",
+    "verylongfermata",
+)
