c1a5f0679004281beedc44411be9506abc351e2a
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-06-08 22:08:50 +0200

    Set to 10.3 + cleanup

--- main.py
+++ main.py
@@ -7,230 +7,39 @@ from mutwo import core_converters
 from mutwo import clock_converters
 from mutwo import clock_events
 from mutwo import clock_interfaces
-from mutwo import common_generators
 from mutwo import core_events
-from mutwo import diary_converters
 from mutwo import project_converters
 from mutwo import timeline_interfaces
 
 import project
 
 
-def make_clock(poem_index, poem_line, before_rest_duration=0) -> clock_interfaces.Clock:
-    print("make clock for", poem_line, "...")
-
-    scale = project.constants.PENTATONIC_SCALE_TUPLE[poem_index]
-
-    match poem_index % 4:
-        case 0:
-            part_count = 2
-            energy = 61
-        case 1:
-            part_count = 3
-            energy = 51
-        case 2:
-            part_count = 2
-            energy = 41
-        case 3:
-            part_count = 1
-            energy = 51
-
-    if not poem_line:
-        scale_position_tuple = tuple((0, 0) for _ in range(part_count * 4))
-    else:
-        markov_chain = scale_to_markov_chain(scale)
-        g = markov_chain.walk_deterministic(tuple(markov_chain.keys())[0])
-
-        scale_position_list = []
-        for _ in range(part_count):
-            scale_position_list.extend(next(g))
-
-        scale_position_tuple = tuple(scale_position_list)
-
-    root_pitch_tuple = tuple(
-        scale.scale_position_to_pitch(scale_position)
-        for scale_position in scale_position_tuple
-    )
-
-    modal_sequential_event = core_events.SequentialEvent(
-        [
-            clock_events.ModalEvent0(start_pitch, end_pitch, scale, energy=energy)
-            for start_pitch, end_pitch in zip(root_pitch_tuple, root_pitch_tuple[1:])
-        ]
-    )
+def make_clock(before_rest_duration=0) -> clock_interfaces.Clock:
+    modal_sequential_event = core_events.SequentialEvent([])
 
     project.clocks.apply_clock_events(modal_sequential_event)
-
-    if part_count >= 2:
-        insert_modal_event(
-            modal_sequential_event,
-            scale,
-            energy=-2,
-            index=7,
-            duration=f(8, 16),
-        )
-
-    if part_count >= 2:
-        insert_modal_event(
-            modal_sequential_event,
-            scale,
-            energy=-2,
-            index=3,
-            duration=f(8, 16),
-        )
 
     for modal_event in modal_sequential_event:
         modal_event.control_event = core_events.SimultaneousEvent(
             [core_events.SequentialEvent([core_events.SimpleEvent(1)])]
         )
 
-    main_clock_line = clock_converters.Modal0SequentialEventToClockLine(
-        (
-            diary_converters.Modal0SequentialEventToEventPlacementTuple(
-                orchestration=project.constants.ORCHESTRATION.get_subset("PCLOCK"),
-                add_mod1=True,
-            ),
-            diary_converters.Modal0SequentialEventToEventPlacementTuple(
-                orchestration=project.constants.ORCHESTRATION.get_subset(
-                    "GLOCKENSPIEL"
-                ),
-                add_mod1=True,
-            ),
-            diary_converters.Modal0SequentialEventToEventPlacementTuple(
-                orchestration=project.constants.ORCHESTRATION.get_subset("HARP"),
-                add_mod1=True,
-            ),
-            diary_converters.Modal0SequentialEventToEventPlacementTuple(
-                orchestration=project.constants.ORCHESTRATION.get_subset("V"),
-                add_mod1=True,
-            ),
-            diary_converters.Modal0SequentialEventToEventPlacementTuple(
-                orchestration=project.constants.ORCHESTRATION.get_subset("V", "HARP"),
-                add_mod1=False,
-            ),
-            diary_converters.Modal0SequentialEventToEventPlacementTuple(
-                orchestration=project.constants.ORCHESTRATION.get_subset(
-                    "GLOCKENSPIEL",
-                    "V",
-                    "HARP",
-                ),
-                add_mod1=False,
-            ),
-        )
-    ).convert(modal_sequential_event)
+    main_clock_line = clock_converters.Modal0SequentialEventToClockLine(()).convert(
+        modal_sequential_event
+    )
 
     # Fix overlaps
     main_clock_line.resolve_conflicts(
         [
-            TuningForkHitStrategy(),
-            TagCountStrategy(),
             timeline_interfaces.AlternatingStrategy(),
         ],
-        is_conflict=is_conflict,
     )
 
-    start_clock_line = (
-        _clock_rest(before_rest_duration) if before_rest_duration > 0 else None
-    )
-    end_clock_line = _clock_end(scale)
+    start_clock_line = None
+    end_clock_line = None
     clock = clock_interfaces.Clock(main_clock_line, start_clock_line, end_clock_line)
 
     return clock
-
-
-def is_conflict(event_placement_0, event_placement_1):
-    tag_tuple0, tag_tuple1 = (
-        ep.tag_tuple for ep in (event_placement_0, event_placement_1)
-    )
-
-    # Avoid overlaps between bowed tuning forks and other percussion
-    # sounds (because the player need to hold the tuning fork with one
-    # hand).
-    if may_be_bowed_tuning_fork_conflict(event_placement_0, event_placement_1):
-        for n in get_overlapping_tuning_fork_note_tuple(
-            event_placement_0, event_placement_1
-        ):
-            if (
-                hasattr(n, "notation_indicator_collection")
-                and n.notation_indicator_collection.duration_line.is_active
-            ):
-                return True
-
-    # Standard logic
-    share_instruments = bool(set(tag_tuple0).intersection(set(tag_tuple1)))
-    return share_instruments
-
-
-def may_be_bowed_tuning_fork_conflict(event_placement_0, event_placement_1):
-    tag_tuple0, tag_tuple1 = (
-        ep.tag_tuple for ep in (event_placement_0, event_placement_1)
-    )
-    return (
-        project.constants.ORCHESTRATION.PCLOCK.name in tag_tuple0
-        and project.constants.ORCHESTRATION.GLOCKENSPIEL.name in tag_tuple1
-    ) or (
-        project.constants.ORCHESTRATION.PCLOCK.name in tag_tuple1
-        and project.constants.ORCHESTRATION.GLOCKENSPIEL.name in tag_tuple0
-    )
-
-
-def get_overlapping_tuning_fork_note_tuple(event_placement_0, event_placement_1):
-    glockenspiel_event, glockenspiel_ep = get_glockenspiel_event(
-        event_placement_0, event_placement_1
-    )
-    adjusted_glockenspiel_event = glockenspiel_event.set(
-        "duration", glockenspiel_ep.duration, mutate=False
-    )
-    _, pclock_ep = get_pclock_event(event_placement_0, event_placement_1)
-    pclock_duration_range = pclock_ep.time_range
-    overlapping_tuning_fork_note_list = []
-    for sequential_event_copy, sequential_event in zip(
-        # We need the duration information from the copied event
-        # (because we only copied the event in order to scale to real
-        # duration), but want to have the original note (because this
-        # is the one which needs to be adjusted).
-        adjusted_glockenspiel_event,
-        glockenspiel_event,
-    ):
-        (
-            absolute_time_tuple,
-            duration,
-        ) = sequential_event_copy._absolute_time_tuple_and_duration
-        for start, end, n in zip(
-            absolute_time_tuple, absolute_time_tuple[1:] + (duration,), sequential_event
-        ):
-            r_start = start + glockenspiel_ep.min_start
-            r_end = end + glockenspiel_ep.min_start
-            if r_start in pclock_duration_range or r_end in pclock_duration_range:
-                overlapping_tuning_fork_note_list.append(n)
-    return tuple(overlapping_tuning_fork_note_list)
-
-
-def get_glockenspiel_event(event_placement_0, event_placement_1):
-    return get_tag_event(
-        event_placement_0,
-        event_placement_1,
-        project.constants.ORCHESTRATION.GLOCKENSPIEL.name,
-    )
-
-
-def get_pclock_event(event_placement_0, event_placement_1):
-    return get_tag_event(
-        event_placement_0,
-        event_placement_1,
-        project.constants.ORCHESTRATION.PCLOCK.name,
-    )
-
-
-def get_tag_event(event_placement_0, event_placement_1, tag):
-    tag_tuple0, tag_tuple1 = (
-        ep.tag_tuple for ep in (event_placement_0, event_placement_1)
-    )
-    if tag in tag_tuple0:
-        ep = event_placement_0
-    else:
-        ep = event_placement_1
-    return ep.event[tag], ep
 
 
 def scale_to_markov_chain(scale):
@@ -283,60 +92,6 @@ def _clock_rest(rest_duration):
     )
 
 
-def _clock_end(scale):
-    modal_sequential_event = core_events.SequentialEvent(
-        [
-            clock_events.ModalEvent0(
-                scale.scale_index_to_pitch(0), scale.scale_index_to_pitch(0), scale
-            ).set("is_end", True)
-        ]
-    )
-    project.clocks.apply_clock_events(modal_sequential_event)
-    return clock_converters.Modal0SequentialEventToClockLine([]).convert(
-        modal_sequential_event
-    )
-
-
-class TuningForkHitStrategy(timeline_interfaces.ConflictResolutionStrategy):
-    def __init__(self, fix_level: int = 5):
-        # fix_level = 10    => always fix
-        # fix_level = 0     => never fix
-        self.fix_level = fix_level
-        self.activity_level = common_generators.ActivityLevel()
-
-    def resolve_conflict(self, timeline, conflict) -> bool:
-        event_placement_0, event_placement_1 = conflict.left, conflict.right
-        if may_be_bowed_tuning_fork_conflict(event_placement_0, event_placement_1):
-            # Don't fix always, because this would be a bit boring :)
-            if self.activity_level(self.fix_level):
-                self._solve(event_placement_0, event_placement_1)
-                return True
-        return False
-
-    def _solve(self, event_placement_0, event_placement_1):
-        for n in get_overlapping_tuning_fork_note_tuple(
-            event_placement_0, event_placement_1
-        ):
-            try:
-                n.notation_indicator_collection.duration_line.is_active = False
-            except AttributeError:
-                pass
-
-
-class TagCountStrategy(timeline_interfaces.TagCountStrategy):
-    def __init__(self, prefer_more_level: int = 8):
-        self.prefer_more_level = prefer_more_level
-        self.activity_level = common_generators.ActivityLevel()
-        self.tag_count_more = timeline_interfaces.TagCountStrategy()
-        self.tag_count_fewer = timeline_interfaces.TagCountStrategy(False)
-
-    def resolve_conflict(self, timeline, conflict) -> bool:
-        if self.activity_level(self.prefer_more_level):
-            return self.tag_count_more.resolve_conflict(timeline, conflict)
-        else:
-            return self.tag_count_fewer.resolve_conflict(timeline, conflict)
-
-
 if __name__ == "__main__":
     import argparse
 
@@ -360,11 +115,8 @@ if __name__ == "__main__":
 
     with diary_interfaces.open():
         clock_list = []
-        for index, line in enumerate(project.constants.POEM.split("\n")):
-            if index < max_index:
-                clock_list.append(make_clock(index, line))
-            else:
-                break
+        for _ in range(1):
+            clock_list.append(make_clock())
 
     clock_tuple = tuple(clock_list)
 
--- project/render/illustration.py
+++ project/render/illustration.py
@@ -6,136 +6,24 @@ import jinja2
 import ranges
 
 from mutwo import abjad_converters
-from mutwo import core_events
-from mutwo import music_events
-from mutwo import music_parameters
-
-import project
 
 
 def illustration():
     base_path = "builds/illustrations"
-    harp_scordatura_path = f"{base_path}/harp_tuning.png"
-    v_scordatura_path = f"{base_path}/v_tuning.png"
-    glockenspiel_scordatura_path = f"{base_path}/glockenspiel_tuning.png"
     intro_tex_path = f"{base_path}/intro.tex"
-    poem_path = f"{base_path}/poem.tex"
 
     try:
         os.mkdir(base_path)
     except FileExistsError:
         pass
 
-    illustrate_poem(poem_path)
-    illustrate_harp_tuning(project.constants.ORCHESTRATION, harp_scordatura_path)
-    illustrate_v_tuning(project.constants.ORCHESTRATION, v_scordatura_path)
-    illustrate_glockenspiel_tuning(
-        project.constants.ORCHESTRATION, glockenspiel_scordatura_path
-    )
     illustrate_start(
         intro_tex_path,
-        harp_scordatura_path,
-        glockenspiel_scordatura_path,
-        v_scordatura_path,
     )
 
 
-def illustrate_harp_tuning(orchestration, path):
-    pitch_range = ranges.Range(
-        music_parameters.JustIntonationPitch("7/12"),
-        music_parameters.JustIntonationPitch("7/6"),
-    )
-    pitch_tuple = tuple(p for p in orchestration.HARP.pitch_tuple if p in pitch_range)
-    illustrate_tuning(path, pitch_tuple)
-
-
-def illustrate_glockenspiel_tuning(orchestration, path):
-    pitch_range = ranges.Range(
-        music_parameters.JustIntonationPitch("1/2"),
-        music_parameters.JustIntonationPitch("1/1"),
-    )
-    pitch_tuple = tuple(
-        p for p in orchestration.GLOCKENSPIEL.pitch_tuple if p in pitch_range
-    )
-    illustrate_tuning(path, pitch_tuple, clef_name="treble")
-
-
-def illustrate_v_tuning(orchestration, path):
-    pitch_tuple = tuple(s.tuning for s in orchestration.V.string_tuple)
-    illustrate_tuning(path, pitch_tuple, clef_name="bass")
-
-
-def illustrate_tuning(path, pitch_tuple, clef_name=None, resolution=400, title=None):
-    seq = core_events.SequentialEvent(
-        [music_events.NoteLike(p, 1) for p in pitch_tuple]
-    )
-    if clef_name:
-        seq[0].notation_indicator_collection.clef.name = clef_name
-    for notelike in seq:
-        c = round(
-            notelike.pitch_list[0].cent_deviation_from_closest_western_pitch_class, 1
-        )
-        notelike.notation_indicator_collection.markup.content = (
-            rf"\markup {{ \typewriter \teeny { c } }}"
-        )
-        notelike.notation_indicator_collection.markup.direction = abjad.enums.UP
-
-    seq2avoice = abjad_converters.SequentialEventToAbjadVoice(
-        mutwo_pitch_to_abjad_pitch=abjad_converters.MutwoPitchToHEJIAbjadPitch(),
-        mutwo_volume_to_abjad_attachment_dynamic=None,
-        tempo_envelope_to_abjad_attachment_tempo=None,
-    )
-    abjad_staff = abjad.Staff([seq2avoice.convert(seq)])
-    leaf_sequence = abjad.select.leaves(abjad_staff)
-    first_leaf = leaf_sequence[0]
-    abjad.attach(
-        abjad.LilyPondLiteral(
-            r"\omit Staff.BarLine "
-            r"\omit Staff.TimeSignature "
-            r'\accidentalStyle "dodecaphonic" '
-        ),
-        first_leaf,
-    )
-    abjad_score = abjad.Score([abjad_staff])
-    lilypond_file = abjad.LilyPondFile()
-    lilypond_file.items.append(
-        "\n".join(
-            (
-                r'\include "etc/lilypond/ekme-heji.ily"',
-                r'\include "lilypond-book-preamble.ly"',
-                r"#(ly:set-option 'tall-page-formats 'png)",
-            )
-        )
-    )
-    if title:
-        header = abjad.Block("header")
-        header.items.append(
-            r"title = \markup { \fontsize #-5 \medium \typewriter { "
-            f'"{title}"'
-            r" } }"
-        )
-        lilypond_file.items.append(header)
-    lilypond_file.items.append(abjad_score)
-    return abjad.persist.as_png(lilypond_file, path, resolution=resolution)
-
-
-def illustrate_start(
-    tex_path, harp_scordatura, glockenspiel_scordatura_path, v_scordatura_path
-):
-    template = J2ENVIRONMENT.get_template("intro.tex.j2").render(
-        harp_scordatura=harp_scordatura,
-        glockenspiel_scordatura=glockenspiel_scordatura_path,
-        v_scordatura=v_scordatura_path,
-    )
-    with open(tex_path, "w") as b:
-        b.write(template)
-    call_latex(tex_path)
-
-
-def illustrate_poem(tex_path):
-    template = J2ENVIRONMENT.get_template("poem.tex.j2").render(
-        poem=project.constants.POEM.split("\n")
-    )
+def illustrate_start(tex_path):
+    template = J2ENVIRONMENT.get_template("intro.tex.j2").render()
     with open(tex_path, "w") as b:
         b.write(template)
     call_latex(tex_path)
