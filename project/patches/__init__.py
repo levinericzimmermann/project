66151675454b5e5145c521d2de31e99801652cf5
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-06-10 14:19:45 +0200

    fixup! Drop upstream applied patches

--- project/patches/__init__.py
+++ project/patches/__init__.py
@@ -1,7 +1,5 @@
 from . import abjad
-from . import core_events
 from . import music_parameters
 from . import notation_indicators
 from . import playing_indicators
 from . import midi
-from . import clocks
