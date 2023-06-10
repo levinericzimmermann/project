2847cb92f122c7156e4918047b52eb095d8a1a3b
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-06-10 16:34:21 +0200

    h130s: Use alsmost full duration, use duration line

--- project/entries/dynamic/h103/h130s.py
+++ project/entries/dynamic/h103/h130s.py
@@ -20,14 +20,18 @@ def main(
 ) -> timeline_interfaces.EventPlacement:
     duration = context.end - context.start
 
-    real_duration = fractions.Fraction(22, 16)
-    if real_duration > duration:
-        real_duration = duration - fractions.Fraction(1, 16)
+    # real_duration = fractions.Fraction(22, 16)
+    # if real_duration > duration:
+    #     real_duration = duration - fractions.Fraction(1, 16)
+    real_duration = duration - fractions.Fraction(1, 4)
+    # real_duration = fractions.Fraction(1, 1)
 
     start_range, end_range = project_utilities.get_ranges(real_duration, duration, 0.5)
 
+    n = music_events.NoteLike(context.pitch, 1)
+    n.notation_indicator_collection.duration_line.is_active = True
     sequential_event = core_events.SequentialEvent(
-        [music_events.NoteLike(context.pitch, 1)]
+        [n]
     )
 
     simultaneous_event = core_events.TaggedSimultaneousEvent(
