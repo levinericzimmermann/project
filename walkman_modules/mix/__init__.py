9dd66301907d6d2e047676863e175358d827409c
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-06-20 11:59:11 +0200

    walkman += resonators
    We add static resonators in order to make a more diffuse sweet sound.
    Frequencies should be mapped to structure of pages (pending TODO).

--- walkman_modules/mix/__init__.py
+++ walkman_modules/mix/__init__.py
@@ -1,18 +1,22 @@
+from __future__ import annotations
 import functools
 import operator
+import typing
 
 import numpy as np
 import pyo
 import walkman
 
-MAX_INPUT_CHANNEL_COUNT = 10
+MAX_INPUT_CHANNEL_COUNT = 5
 
 
 class Diffusion(
     walkman.ModuleWithDecibel,
     **{
         f"audio_input_{index}": walkman.Catch(
-            walkman.constants.EMPTY_MODULE_INSTANCE_NAME, implicit=False
+            # set implicit=True so that it's sufficient to activate 'Diffusion' in order
+            # to run all inputs.
+            walkman.constants.EMPTY_MODULE_INSTANCE_NAME, implicit=True
         )
         for index in range(MAX_INPUT_CHANNEL_COUNT)
     },
@@ -127,3 +131,124 @@ class PyoObjectMixer(pyo.PyoObject):
 
 def decibel2amp(decibel):
     return float(1 * (10 ** (decibel / 20)))
+
+
+# Please see https://cnmat.berkeley.edu/sites/default/files/patches/Picture%203_0.png
+Frequency = float
+Amplitude = float
+DecayRate = float
+ResonatorConfiguration = typing.Tuple[Frequency, Amplitude, DecayRate]
+FilePath = str
+SliceIndex = int
+ResonanceConfigurationFilePathList = typing.List[
+    typing.Union[
+        typing.Tuple[Amplitude, FilePath], typing.Tuple[Amplitude, FilePath, SliceIndex]
+    ]
+]
+
+
+class Resonator(
+    walkman.ModuleWithDecibel,
+    decibel=walkman.AutoSetup(walkman.Parameter),
+    audio_input=walkman.Catch(walkman.constants.EMPTY_MODULE_INSTANCE_NAME),
+):
+    def __init__(
+        self,
+        resonance_configuration_file_path_list: ResonanceConfigurationFilePathList,
+        input_index: int = 0,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.input_index = input_index
+        self.resonator_configuration_tuple = (
+            self.parse_resonance_configuration_file_path_list(
+                resonance_configuration_file_path_list
+            )
+        )
+
+    @staticmethod
+    def parse_resonance_configuration_file_path_list(
+        resonance_configuration_file_path_list: ResonanceConfigurationFilePathList,
+    ) -> typing.Tuple[
+        typing.Tuple[Amplitude, typing.Tuple[ResonatorConfiguration, ...]]
+    ]:
+        resonator_configuration_list = []
+        for data in resonance_configuration_file_path_list:
+            try:
+                amplitude, resonance_configuration_file_path, slice_index = data
+            except ValueError:
+                amplitude, resonance_configuration_file_path = data
+                slice_index = 1
+            resonance_configuration_tuple = (
+                Resonator.parse_resonance_configuration_file(
+                    resonance_configuration_file_path
+                )
+            )[::slice_index]
+            resonator_configuration_list.append(
+                (amplitude, resonance_configuration_tuple)
+            )
+        return tuple(resonator_configuration_list)
+
+    @staticmethod
+    def parse_resonance_configuration_file(
+        resonance_configuration_file_path: str,
+    ) -> typing.Tuple[ResonatorConfiguration, ...]:
+        with open(
+            resonance_configuration_file_path, "r"
+        ) as resonance_configuration_file:
+            configuration_content = resonance_configuration_file.read()
+
+        resonance_configuration_list = []
+        for configuration_line in configuration_content.splitlines():
+            try:
+                _, data = configuration_line.replace(";", "").split(",")
+            except ValueError:
+                continue
+            frequency, amplitude, decay = (
+                float(value) for value in filter(bool, data.split(" "))
+            )
+            resonance_configuration_list.append((frequency, amplitude, decay))
+        # XXX: We need set because in the text files are duplicates.
+        return tuple(set(resonance_configuration_list))
+
+    def _setup_pyo_object(self):
+        super()._setup_pyo_object()
+        # XXX: It's better to apply amplitude on input instead
+        # on resonators, for less sudden fade in.
+        self.audio_input_with_applied_decibel = (
+            self.audio_input.pyo_object[self.input_index]
+        )
+        self.resonator_list = []
+        for (
+            amplitude,
+            resonance_configuration_tuple,
+        ) in self.resonator_configuration_tuple:
+            frequency_list, amplitude_list, decay_list = (
+                list(value_list) for value_list in zip(*resonance_configuration_tuple)
+            )
+            complex_resonator = pyo.ComplexRes(
+                self.audio_input_with_applied_decibel,
+                freq=frequency_list,
+                decay=decay_list,
+                mul=amplitude_list,
+            ).stop()
+            mixed_resonator = complex_resonator.mix(1)
+            if amplitude != 1:
+                complex_resonator_with_applied_amplitude = mixed_resonator * amplitude
+            else:
+                complex_resonator_with_applied_amplitude = mixed_resonator
+            self.resonator_list.append(complex_resonator_with_applied_amplitude)
+            self.internal_pyo_object_list.append(complex_resonator)
+
+        self.summed_resonator = sum(self.resonator_list) * self.amplitude_signal_to
+
+        internal_pyo_object_list = [
+            self.summed_resonator,
+            self.audio_input_with_applied_decibel,
+        ] + self.resonator_list
+
+        self.internal_pyo_object_list.extend(internal_pyo_object_list)
+
+    @property
+    def _pyo_object(self) -> pyo.PyoObject:
+        return self.summed_resonator
