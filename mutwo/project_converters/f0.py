b7e08e988db50447a046f708c4e6d6784ad4bf6e
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-06-25 13:02:26 +0200

    converters/f0: differentiate between abstract data calculation and real event generation => this may differ for different purposes

--- mutwo/project_converters/f0.py
+++ mutwo/project_converters/f0.py
@@ -1,3 +1,5 @@
+import abc
+
 import numpy as np
 
 from mutwo import common_generators
@@ -6,7 +8,61 @@ from mutwo import core_utilities
 from mutwo import project_converters
 
 
+class DataToF0(core_converters.abc.Converter):
+    @abc.abstractmethod
+    def convert(
+        self, duration_tuple: tuple[int, ...], frequency: float, velocity: int
+    ) -> tuple[str]:
+        ...
+
+
+class DataToContinousF0(DataToF0):
+    def convert(
+        self, duration_tuple: tuple[int, ...], frequency: float, velocity: int
+    ) -> tuple[str]:
+        event_count = len(duration_tuple)
+
+        # If we do have enough events we'll interpolate to our max
+        # volume to make a rising/falling envelope.
+        # This is necessary, because the DSP of the Arduino (f0.ino)
+        # doesn't do this by themselves.
+        v_tuple = tuple(velocity for _ in duration_tuple)
+        if event_count >= 3 and frequency != 0:
+            min_v = min((project_converters.constants.F0.MIN_VELOCITY, velocity))
+            left, center, right = common_generators.euclidean(event_count, 3)
+            v_tuple = (
+                tuple(np.geomspace(min_v, velocity, left, dtype=int))
+                + tuple(velocity for _ in range(center))
+                + tuple(np.geomspace(velocity, min_v, right, dtype=int))
+            )
+
+        assert len(v_tuple) == event_count
+
+        e_list = []
+
+        max_index = len(duration_tuple) - 1
+        for index, ed in enumerate(duration_tuple):
+            if index == 0:
+                state = project_converters.constants.F0.STATE_NEW
+            elif index == max_index:
+                state = project_converters.constants.F0.STATE_STOP
+            else:
+                state = project_converters.constants.F0.STATE_KEEP
+
+            # state,duration,frequency,velocity
+            e_list.append(
+                project_converters.constants.F0.PARAMETER_DELIMITER.join(
+                    [str(p) for p in (state, ed, frequency, v_tuple[index])]
+                )
+            )
+
+        return (project_converters.constants.F0.EVENT_DELIMITER.join(e_list),)
+
+
 class EventToF0(core_converters.abc.EventConverter):
+    def __init__(self, data_to_continous_f0: DataToContinousF0 = DataToContinousF0()):
+        self._data_to_continous_f0 = data_to_continous_f0
+
     def convert(self, event_to_convert):
         return self._convert_event(event_to_convert, 0)[0]
 
@@ -17,8 +73,6 @@ class EventToF0(core_converters.abc.EventConverter):
         d_tuple = common_generators.euclidean(
             d, max((d // project_converters.constants.F0.MAX_DURATION, 2))
         )
-
-        e_count = len(d_tuple)
 
         try:
             f = round(e.pitch_list[0].frequency, 2)
@@ -32,41 +86,9 @@ class EventToF0(core_converters.abc.EventConverter):
         else:
             v = int(core_utilities.scale(v, 0, 127, 0, 255))
 
-        # If we do have enough events we'll interpolate to our max
-        # volume to make a rising/falling envelope.
-        # This is necessary, because the DSP of the Arduino (f0.ino)
-        # doesn't do this by themselves
-        v_tuple = tuple(v for _ in d_tuple)
-        if e_count >= 3 and f != 0:
-            min_v = min((project_converters.constants.F0.MIN_VELOCITY, v))
-            left, center, right = common_generators.euclidean(e_count, 3)
-            v_tuple = (
-                tuple(np.geomspace(min_v, v, left, dtype=int))
-                + tuple(v for _ in range(center))
-                + tuple(np.geomspace(v, min_v, right, dtype=int))
-            )
+        data = (d_tuple, f, v)
 
-        assert len(v_tuple) == e_count
-
-        e_list = []
-
-        max_index = len(d_tuple) - 1
-        for index, ed in enumerate(d_tuple):
-            if index == 0:
-                state = project_converters.constants.F0.STATE_NEW
-            elif index == max_index:
-                state = project_converters.constants.F0.STATE_STOP
-            else:
-                state = project_converters.constants.F0.STATE_KEEP
-
-            # state,duration,frequency,velocity
-            e_list.append(
-                project_converters.constants.F0.PARAMETER_DELIMITER.join(
-                    [str(p) for p in (state, ed, f, v_tuple[index])]
-                )
-            )
-
-        return (project_converters.constants.F0.EVENT_DELIMITER.join(e_list),)
+        return self._data_to_continous_f0(*data)
 
     def _convert_sequential_event(self, *args, **kwargs):
         return (
