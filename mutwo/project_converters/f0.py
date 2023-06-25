02e3a487cb006b5c7d8c4d84805bb3b9124eaaf4
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-06-25 13:22:40 +0200

    converters/f0 += percussive driver

--- mutwo/project_converters/f0.py
+++ mutwo/project_converters/f0.py
@@ -8,6 +8,39 @@ from mutwo import core_utilities
 from mutwo import project_converters
 
 
+# Basic parsing of converted parameters to F0 form
+#
+# Declare the converters as singleton
+
+
+class DataToF0Event(core_converters.abc.Converter):
+    def convert(self, state: int, duration: int, frequency: float, velocity: int):
+        # state,duration,frequency,velocity
+        return project_converters.constants.F0.PARAMETER_DELIMITER.join(
+            [str(p) for p in (state, duration, frequency, velocity)]
+        )
+
+
+class F0EventListToF0(core_converters.abc.Converter):
+    def convert(self, f0_event_list: list[str]) -> str:
+        return project_converters.constants.F0.EVENT_DELIMITER.join(f0_event_list)
+
+
+DATA_TO_F0_EVENT = DataToF0Event()
+F0_EVENT_LIST_TO_F0 = F0EventListToF0()
+
+
+# Now
+#   mutwo -> f0
+# parsing
+#
+# We have different strategies, depending on the musical material
+#
+#   DataToF0    (abstract)
+#       DataToContinousF0       => continous tones
+#       DataToPercussiveF0      => short 'staccatto' tones
+
+
 class DataToF0(core_converters.abc.Converter):
     @abc.abstractmethod
     def convert(
@@ -19,13 +52,13 @@ class DataToF0(core_converters.abc.Converter):
 class DataToContinousF0(DataToF0):
     def convert(
         self, duration_tuple: tuple[int, ...], frequency: float, velocity: int
-    ) -> tuple[str]:
+    ) -> str:
         event_count = len(duration_tuple)
 
         # If we do have enough events we'll interpolate to our max
         # volume to make a rising/falling envelope.
         # This is necessary, because the DSP of the Arduino (f0.ino)
-        # doesn't do this by themselves.
+        # doesn't do this by itself.
         v_tuple = tuple(velocity for _ in duration_tuple)
         if event_count >= 3 and frequency != 0:
             min_v = min((project_converters.constants.F0.MIN_VELOCITY, velocity))
@@ -49,19 +82,39 @@ class DataToContinousF0(DataToF0):
             else:
                 state = project_converters.constants.F0.STATE_KEEP
 
-            # state,duration,frequency,velocity
-            e_list.append(
-                project_converters.constants.F0.PARAMETER_DELIMITER.join(
-                    [str(p) for p in (state, ed, frequency, v_tuple[index])]
-                )
-            )
+            e_list.append(DATA_TO_F0_EVENT(state, ed, frequency, v_tuple[index]))
 
-        return (project_converters.constants.F0.EVENT_DELIMITER.join(e_list),)
+        return F0_EVENT_LIST_TO_F0(e_list)
+
+
+class DataToPercussiveF0(core_converters.abc.Converter):
+    def convert(
+        self, duration_tuple: tuple[int, ...], frequency: float, velocity: int
+    ) -> str:
+        # We only need one active event & we can set everything else to 'Rest'
+        e_list = [
+            DATA_TO_F0_EVENT(
+                project_converters.constants.F0.STATE_NEW,
+                duration_tuple[0],
+                frequency,
+                velocity,
+            )
+        ]
+        for d in duration_tuple[1:]:
+            e_list.append(
+                DATA_TO_F0_EVENT(project_converters.constants.F0.STATE_STOP, d, 0, 0)
+            )
+        return F0_EVENT_LIST_TO_F0(e_list)
 
 
 class EventToF0(core_converters.abc.EventConverter):
-    def __init__(self, data_to_continous_f0: DataToContinousF0 = DataToContinousF0()):
+    def __init__(
+        self,
+        data_to_continous_f0: DataToContinousF0 = DataToContinousF0(),
+        data_to_percussive_f0: DataToPercussiveF0 = DataToPercussiveF0(),
+    ):
         self._data_to_continous_f0 = data_to_continous_f0
+        self._data_to_percussive_f0 = data_to_percussive_f0
 
     def convert(self, event_to_convert):
         return self._convert_event(event_to_convert, 0)[0]
@@ -88,13 +141,11 @@ class EventToF0(core_converters.abc.EventConverter):
 
         data = (d_tuple, f, v)
 
-        return self._data_to_continous_f0(*data)
+        return (self._data_to_continous_f0(*data),)
 
     def _convert_sequential_event(self, *args, **kwargs):
         return (
-            project_converters.constants.F0.EVENT_DELIMITER.join(
-                super()._convert_sequential_event(*args, **kwargs)
-            ),
+            F0_EVENT_LIST_TO_F0(super()._convert_sequential_event(*args, **kwargs)),
         )
 
     def _convert_simultaneous_event(
