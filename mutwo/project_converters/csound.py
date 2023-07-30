42c8e7eb0efd7f611a976df45b1318fe8a5aca2f
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-07-30 19:19:26 +0200

    fixup! ensure harmonies are more stable and interpolation times are shorter

--- mutwo/project_converters/csound.py
+++ mutwo/project_converters/csound.py
@@ -46,6 +46,73 @@ class PitchTupleToSoundFile(csound_converters.EventToSoundFile):
         return self._render_mp3(path)
 
     def _get_sequential_event(self, pitch_tuple, duration, duration_per_harmony):
+        interpolation_list = self._get_interpolation_list(pitch_tuple)
+        harmony_tick_event = core_events.SequentialEvent(
+            [core_events.SimpleEvent(d) for d in duration_per_harmony]
+        )
+        abs_tt = harmony_tick_event.absolute_time_tuple
+
+        sequential_event = core_events.SequentialEvent([])
+        while sequential_event.duration < duration:
+            pitch = self._get_pitch(
+                abs_tt,
+                sequential_event,
+                harmony_tick_event,
+                pitch_tuple,
+                duration_per_harmony,
+                interpolation_list,
+            )
+            note = self._get_note(pitch)
+            sequential_event.append(note)
+
+        sequential_event.cut_out(0, duration)
+
+        if not (pl := sequential_event[-1].pitch_list):
+            pl.append(pitch_tuple[-1])
+
+        return sequential_event
+
+    def _get_pitch(
+        self,
+        abs_tt,
+        sequential_event,
+        harmony_tick_event,
+        pitch_tuple,
+        duration_per_harmony,
+        interpolation_list,
+    ):
+        d = sequential_event.duration
+        harmony_tick_index = harmony_tick_event.get_event_index_at(d)
+        c = harmony_tick_index // 2
+        if harmony_tick_index % 2 == 0:
+            pitch = pitch_tuple[c]
+        else:
+            interpolation_dur = duration_per_harmony[harmony_tick_index]
+            position = (d - abs_tt[harmony_tick_index]) / interpolation_dur
+            pitch = interpolation_list[c].gamble_at(position)
+
+        pitch = self._random.choice(self.AMBITUS.get_pitch_variant_tuple(pitch))
+        return pitch
+
+    def _get_note(self, pitch):
+        drange = (30, 100)
+        if not self._activity_level(3):
+            pitch = None
+            drange = (15, 50)
+        note_duration = self._random.uniform(*drange)
+        if self._activity_level(5):
+            decibel = -12
+        else:
+            decibel = -36
+        if not pitch:
+            decibel = -100
+        return music_events.NoteLike(
+            pitch,
+            duration=note_duration,
+            volume=music_parameters.DecibelVolume(decibel),
+        )
+
+    def _get_interpolation_list(self, pitch_tuple):
         interpolation_list = []
         for p0, p1 in zip(pitch_tuple, pitch_tuple[1:]):
             dynamic_choice = core_generators.DynamicChoice(
@@ -56,49 +123,7 @@ class PitchTupleToSoundFile(csound_converters.EventToSoundFile):
                 ),
             )
             interpolation_list.append(dynamic_choice)
-
-        harmony_tick_event = core_events.SequentialEvent(
-            [core_events.SimpleEvent(d) for d in duration_per_harmony]
-        )
-        abs_tt = harmony_tick_event.absolute_time_tuple
-
-        sequential_event = core_events.SequentialEvent([])
-        while sequential_event.duration < duration:
-            d = sequential_event.duration
-            harmony_tick_index = harmony_tick_event.get_event_index_at(d)
-            c = harmony_tick_index // 2
-            if harmony_tick_index % 2 == 0:
-                pitch = pitch_tuple[c]
-            else:
-                interpolation_dur = duration_per_harmony[harmony_tick_index]
-                position = (d - abs_tt[harmony_tick_index]) / interpolation_dur
-                pitch = dynamic_choice.gamble_at(position)
-
-            pitch = self._random.choice(self.AMBITUS.get_pitch_variant_tuple(pitch))
-            drange = (30, 100)
-            if not self._activity_level(3):
-                pitch = None
-                drange = (15, 50)
-            note_duration = self._random.uniform(*drange)
-            if self._activity_level(5):
-                decibel = -12
-            else:
-                decibel = -36
-            if not pitch:
-                decibel = -100
-            note = music_events.NoteLike(
-                pitch,
-                duration=note_duration,
-                volume=music_parameters.DecibelVolume(decibel),
-            )
-            sequential_event.append(note)
-
-        sequential_event.cut_out(0, duration)
-
-        if not (pl := sequential_event[-1].pitch_list):
-            pl.append(pitch_tuple[-1])
-
-        return sequential_event
+        return interpolation_list
 
     def _render_mp3(self, path):
         mp3path = ".".join(path.split(".")[:-1]) + ".mp3"
