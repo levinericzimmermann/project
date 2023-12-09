e7429b4d06d0772ef6903241f9a0f747105364a7
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-12-09 21:27:22 +0100

    Add basic structure

---
+++ STRUCTURE.md
@@ -0,0 +1,17 @@
+# Structure
+
+- a sequence of slowly unfolding events, sounds
+    - each breath matters
+
+- composed of small parts
+    - each part consist of:
+        - N resonator-algorithms (where n >= 1)
+        - whistling sequence
+        - resonator result (concurrence, that may be longer than the actual part, e.g resonators can overlap)
+
+- there is a global intonation envelope, that keeps track of all sounding intonations
+    - each part writes into this intonation envelope once it's used
+
+- how to order parts? how do parts decide for pitches?
+
+
---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ sources ? import ./nix/sources.nix, rsources ? import (sources.mutwo-nix.outPath + "/nix/sources.nix"), pkgs ? import rsources.nixpkgs {}}:
+
+with pkgs;
+with pkgs.python310Packages;
+
+let
+
+  mutwo-diary   = import (sources.mutwo-nix.outPath + "/mutwo.diary/default.nix") {};
+  mutwo-abjad   = import (sources.mutwo-nix.outPath + "/mutwo.abjad/default.nix") {};
+  mutwo-midi    = import (sources.mutwo-nix.outPath + "/mutwo.midi/default.nix") {};
+  mutwo-mmml    = import ./mutwo.mmml/default.nix {};
+
+  mypython = python310.buildEnv.override {
+    extraLibs = [
+      ipython
+      mutwo-midi
+      mutwo-mmml
+      mutwo-diary
+
+      # Strange bug: mido needs it, but for some reason mutwo.midi
+      # doesn't deliver it.
+      packaging
+    ];
+  };
+
+in
+
+  pkgs.mkShell {
+      buildInputs = with pkgs; [
+          mypython
+
+          # To render documents
+          sile
+      ];
+  }
