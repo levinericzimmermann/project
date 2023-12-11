4c12b32e8455e3ff782e52fa78f62044bd6a6d88
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-12-11 19:34:24 +0100

    events/BreathEvent: set default value for 'breath'

--- mutwo/project_events/breaths.py
+++ mutwo/project_events/breaths.py
@@ -5,7 +5,10 @@ from mutwo import project_parameters
 class BreathEvent(core_events.SimpleEvent):
     def __init__(
         # Each breath is always represented with a duration of '1'
-        self, breath_or_hold_breath: project_parameters.BreathOrHoldBreath, duration=1, **kwargs
+        self,
+        breath_or_hold_breath: project_parameters.BreathOrHoldBreath = project_parameters.Breath(),
+        duration=1,
+        **kwargs
     ):
         super().__init__(duration, **kwargs)
         self.breath_or_hold_breath = breath_or_hold_breath
