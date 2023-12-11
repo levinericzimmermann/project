551d680f0aaa9d93bab0ada0076814a4269bf5cd
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-12-11 19:13:52 +0100

    events/BreathEvent: a breath event could be split, so that it's smaller than duration 1

--- mutwo/project_events/breaths.py
+++ mutwo/project_events/breaths.py
@@ -4,8 +4,8 @@ from mutwo import project_parameters
 
 class BreathEvent(core_events.SimpleEvent):
     def __init__(
-        self, breath_or_hold_breath: project_parameters.BreathOrHoldBreath
-    ):
         # Each breath is always represented with a duration of '1'
-        super().__init__(1)
+        self, breath_or_hold_breath: project_parameters.BreathOrHoldBreath, duration=1, **kwargs
+    ):
+        super().__init__(duration, **kwargs)
         self.breath_or_hold_breath = breath_or_hold_breath
